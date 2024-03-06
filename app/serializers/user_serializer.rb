f3727f98f882f6b5f9390821e6aad6445eb01be4
Thibaud <thibgn@pm.me>
2024-03-06 17:08:35 +0100

    add bruno, fix rubocop

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,8 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at
+  attributes :id, :email, :created_at, :sign_in_count
+
+  attribute :last_sign_in_at do |user|
+    user.last_sign_in_at && user.last_sign_in_at.strftime('%m/%d/%Y')
+  end
 end
